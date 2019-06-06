723e955f4483c721afcf38f3e702f684e8411921
YukihiroMasuoka <yukihiro3socrates6hilbert@gmail.com>
2019-06-07 00:52:02 +0900

    Add function main

--- main.c
+++ main.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
 #include "sin.h"
 
-
 void main(void){
-
+  double x, output;
+  scanf("%lf",&x);
+  output = sin(x);
+  printf("%lf\n",output);
 }
--- sin.h
+++ sin.h
@@ -1,1 +1,6 @@
+#ifndef _SIN_H_
+#define _SIN_H_
+
 double sin(double);
+
+#endif //_SIN_H_
