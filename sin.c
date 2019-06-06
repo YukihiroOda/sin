f5d375e91b8d7713805a3cddaa2c1db84f245a2f
YukihiroMasuoka <yukihiro3socrates6hilbert@gmail.com>
2019-06-06 23:34:00 +0900

    Remove a space

--- sin.c
+++ sin.c
@@ -4,7 +4,7 @@
 double sin(double x) {
   double sum, sgn, coefficient, xx, count, var;
   xx = x*x; var = x;
-  sum = x; sgn = 1.0; coefficient = 1.0; 
+  sum = x; sgn = 1.0; coefficient = 1.0;
   for(count = 1.0 ; count < 100.0; count += 1.00) {
     sgn *= ( -1.0 );
     coefficient *= 1 / ( (2.0 * count) * (2.0 * count + 1.0) );
