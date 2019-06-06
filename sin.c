99640f3b2e14bc61715da553074d167c7a2f148f
YukihiroMasuoka <yukihiro3socrates6hilbert@gmail.com>
2019-06-06 23:30:33 +0900

    Define function sin

--- sin.c
+++ sin.c
@@ -1,7 +1,15 @@
 #include <stdio.h>
 #include "sin.h"
 
-double sin(double x){
-
-  return 0.0;
+double sin(double x) {
+  double sum, sgn, coefficient, xx, count, var;
+  xx = x*x; var = x;
+  sum = x; sgn = 1.0; coefficient = 1.0; 
+  for(count = 1.0 ; count < 100.0; count += 1.00) {
+    sgn *= ( -1.0 );
+    coefficient *= 1 / ( (2.0 * count) * (2.0 * count + 1.0) );
+    var *= xx;
+    sum += (sgn * coefficient * var);
+  }
+  return sum;
 }
