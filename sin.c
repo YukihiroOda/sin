a9dfd42bb8ee27aef3296b386a80a2ef27103b11
YukihiroMasuoka <yukihiro3socrates6hilbert@gmail.com>
2019-06-07 14:27:05 +0900

    Add coments

--- sin.c
+++ sin.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 #include "sin.h"
 
+/* sin (x) = x - 1/3! x^3 + 1/5! x^5 - 1/7! x^7 + ...... */
+/* a_n * var_n =  (-1) * a_{n-1}  * (1/(2n*(2n+1))) * var_{n-1} * x^2   */
+
 double sin(double x) {
   double sum, sgn, coefficient, xx, var;
   int count;
@@ -8,9 +11,10 @@ double sin(double x) {
   sum = x; sgn = 1.0; coefficient = 1.0;
   for(count = 1 ; count < 100; count++) {
     sgn *= ( -1.0 );
-    coefficient *= 1 / ( (2.0 * count) * (2.0 * count + 1.0) );
+    coefficient *= 1.0 / ( (2 * count) * (2 * count + 1) ); // 1/(2n+1)! = 1/(2n-1)! * 1/(2n*(2n+1))
     var *= xx;
     sum += (sgn * coefficient * var);
   }
   return sum;
 }
+
