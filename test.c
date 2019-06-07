f2dad75e71c3859184a9cc2434ce7c111148b07d
YukihiroMasuoka <yukihiro3socrates6hilbert@gmail.com>
2019-06-07 19:13:51 +0900

    Refuct test.c

--- test.c
+++ test.c
@@ -5,7 +5,7 @@
 
 int test1(double x){
     if ( !(sin( Pi - x ) - sin(x) <= DBL_EPSILON ) ) return 1;
-    else if ( !(sin( x + 2*Pi ) - sin(x) <= DBL_EPSILON) ) return 2;
+    else if ( !(sin( x + 2*Pi ) - sin(x) <= DBL_EPSILON ) ) return 2;
     else return 0;
 }
 
@@ -21,15 +21,15 @@ int main(void){
   for(i = 0 ; i < 40000  ; i++ ) {
     if( test1(x) ) {
       printf("error:%d x=%lf\n",test1(x),x);
-      if(test1(x)==1) printf("sin(Pi-x) = %lf sin(x)=%lf \n",sin(Pi-x),sin(x));
-      else if(test1(x)==2) printf("sin(x+2*Pi) = %lf sin(x)=%lf \n",sin(x+2*Pi),sin(x));
+      if(test1(x)==1) printf("sin(Pi-x) = %lf sin(x) = %lf \n",sin(Pi-x),sin(x));
+      else if(test1(x)==2) printf("sin(x+2*Pi) = %lf sin(x) = %lf \n",sin(x+2*Pi),sin(x));
       return 0;
     }
     for(j = 0 ; j < 40000  ; j++ ) {
       y = -Pi/2;
       if( test2(x,y) ) {
-	printf("error:%d x=%lf y=%lf\n",test2(x,y),x,y);
-	printf(" sin(x+y)=%lf sin(x)= %lf  sin(Pi/2-y)=%lf sin(Pi/2-x)= %lf sin(y)= %lf\n",sin(x+y),sin(x),sin(Pi/2-y),sin(Pi/2-x), sin(y));
+	printf("error:%d x = %lf y = %lf\n",test2(x,y),x,y);
+	printf(" sin(x+y) = %lf sin(x) = %lf  sin(Pi/2-y) = %lf sin(Pi/2-x) = %lf sin(y) = %lf\n",sin(x+y),sin(x),sin(Pi/2-y),sin(Pi/2-x), sin(y));
 	return 0;
       }
       y += 0.0001;
