a1aff3b4d3285be4730689fbeef4c7871162e6f8
YukihiroMasuoka <yukihiro3socrates6hilbert@gmail.com>
2019-06-07 15:33:13 +0900

    Improve sin's convergeness

--- sin.c
+++ sin.c
@@ -7,19 +7,18 @@ double Pi = 3.14159265;
 /* a_n * var_n =  (-1) * a_{n-1}  * (1/(2n*(2n+1))) * var_{n-1} * x^2   */
 
 double sin(double x) {
-  double sum, sgn, coefficient, xx, var;
+  double sum, sgn, term, xx;
   int count;
-  while ( !( (-1 * Pi) <= x && x < Pi ) ) {
+  while ( !( -Pi <= x && x < Pi ) ) {
     if ( Pi <= x ) x -= Pi;
     else x += Pi;
   }
-  xx = x*x; var = x;
-  sum = x; sgn = 1.0; coefficient = 1.0;
+  xx = x*x; 
+  sum = x; sgn = 1.0; term = x;
   for(count = 1 ; count < 100; count++) {
     sgn *= ( -1.0 );
-    var *= xx;
-    coefficient *= 1.0 / ( (2 * count) * (2 * count + 1) ); // 1/(2n+1)! = 1/(2n-1)! * 1/(2n*(2n+1))
-    sum += (sgn * coefficient * var );
+    term *= xx / ( (2 * count) * (2 * count + 1) ); // 1/(2n+1)! = 1/(2n-1)! * 1/(2n*(2n+1))
+    sum += (sgn * term);
   }
   return sum;
 }
