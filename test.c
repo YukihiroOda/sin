e8c3a9a196bab4002f717b1566d6c305caf7139d
YukihiroMasuoka <yukihiro3socrates6hilbert@gmail.com>
2019-06-10 09:10:52 +0900

    Improve test

--- test.c
+++ test.c
@@ -15,22 +15,35 @@ int test2pi(double x){
   double check;
   check = sin( x + 2*Pi ) - sin(x);
   if(check < 0) check = -check; //absolute
-    if ( !( check <= 1.0e-12 ) ) return 1;
+  if ( !( check <= 1.0e-12 ) ) return 1;
+  else return 0;
+}
+
+double cos(double x){
+  return sin(Pi/2 - x);
+}
+
+int testAddThm1(double x, double y){
+  double check;
+  check = sin( x + y ) - (sin(x)*cos(y) + cos(x)*sin(y));
+  if(check < 0) check = -check; //absolute
+  if ( !(check <= 1.0e-12) ) return 1;
     else return 0;
 }
 
-int testAddThm(double x, double y){
+int testAddThm2(double x, double y){
   double check;
-  check = sin( x + y ) - (sin(x)*sin(Pi/2 - y) + sin(Pi/2 - x)*sin(y));
+  check = cos( x + y ) - (cos(x)*cos(y) - sin(x)*sin(y));
+  if(check < 0) check = -check; //absolute
   if ( !(check <= 1.0e-12) ) return 1;
-    else return 0;
+  else return 0;
 }
 
 int main(void){
   int i, j;
   double x, y;
   x = -Pi;
-  for(i = 0 ; i < 4000  ; i++ ) {
+  for(i = 0 ; i < 40000  ; i++ ) {
     if( testpi(x) ) {
       printf("failed:testpi x=%.32f\n",x);
       printf("sin(Pi-x) = %.32f , sin(x) = %.32f ,  sin(Pi-x)-sin(x) = %.32f \n",sin(Pi-x),sin(x), sin(Pi-x)-sin(x));
@@ -39,15 +52,19 @@ int main(void){
       printf("failed:test2pi x=%.32f\n",x);
       printf("sin(x+2*Pi) = %.32f , sin(x) = %.32f , sin(x+2*Pi)-sin(x) = %.32f \n",sin(x+2*Pi),sin(x),sin(x+2*Pi) - sin(x));
     }
-    for(j = 0 ; j < 4000  ; j++ ) {
+    for(j = 0 ; j < 40000  ; j++ ) {
       y = -Pi/2;
-      if( testAddThm(x,y) ) {
-	printf("failed:testAddThm x = %.32f y = %.32f\n",x,y);
-	printf(" sin(x+y) = %.32f , sin(x) = %.32f , sin(Pi/2-y) = %.32f , sin(Pi/2-x) = %.32f , sin(y) = %.32f , sin( x + y ) - (sin(x)*sin(Pi/2 - y) + sin(Pi/2 - x)*sin(y)) = %.32f\n ",sin(x+y),sin(x),sin(Pi/2-y),sin(Pi/2-x), sin(y),sin( x + y ) - (sin(x)*sin(Pi/2 - y) + sin(Pi/2 - x)*sin(y)));
+      if( testAddThm1(x,y) ) {
+	printf("failed:testAddThm1 x = %.32f y = %.32f\n",x,y);
+	printf(" sin(x+y) = %.32f , sin(x) = %.32f , cos(y) = %.32f , cos(x) = %.32f , sin(y) = %.32f , sin( x + y ) - (sin(x)*cos(y) + cos(x)*sin(y)) = %.32f\n ",sin(x+y),sin(x),cos(y),cos(x), sin(y),sin( x + y ) - (sin(x)*cos(y) + cos(x)*sin(y)));
       }
-      y += 0.001;
+      if( testAddThm2(x,y) ) {
+	printf("failed:testAddThm2 x = %.32f y = %.32f\n",x,y);
+	printf(" cos(x+y) = %.32f , cos(x) = %.32f , cos(y) = %.32f , sin(x) = %.32f , sin(y) = %.32f , cos(x + y) - (cos(x)*cos(y) - sin(x)*sin(y)) = %.32f\n ",cos(x+y),cos(x),cos(y),sin(x), sin(y),cos( x + y ) - (cos(x)*cos(y) - sin(x)*sin(y)));
+      }
+      y += 0.0001;
     }
-    x += 0.001;
+    x += 0.0001;
   }
   printf("This sin function may be well-defined.\n");
   return 0;
