9908649575a1420475a45b0d1d6760a95e6f336d
YukihiroMasuoka <yukihiro3socrates6hilbert@gmail.com>
2019-06-10 15:21:58 +0900

    Improve main.c

--- main.c
+++ main.c
@@ -2,8 +2,17 @@
 #include "sin.h"
 
 void main(void){
-  double x, output;
-  scanf("%lf",&x);
-  output = sin(x);
-  printf("%lf\n",output);
+  double x;
+  int i;
+  x = -Pi;
+  for(i=0;i <= Pi*1100;i++) {
+    printf("%.12f %.12f\n",x,sin(x));
+    x+=0.001;
+  }
+  printf("0.000000000000 %.12f\n",sin(0.0));
+  x = Pi;
+  for(i=0;i <= Pi*1000;i++) {
+    printf("%.12f %.12f\n",x,sin(x));
+    x-=0.001;
+  }
 }
